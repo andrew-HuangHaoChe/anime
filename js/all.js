ffc30a6f55e629bf660335876064fe295a5ea627
Andrew <klgt13145220@gmail.com>
2021-10-04 02:53:15 +0800

    動畫研究

--- js/all.js
+++ js/all.js
@@ -24,8 +24,9 @@ function animeActive (type, className) {
 }
 
 // icon_group動畫
-function scaleAnimation () {
+function incomeAnimation () {
     var icon_one, icon_two, icon_three;
+
     function clearAnime () {
         if(icon_one) {
             icon_one.seek(icon_one.duration)
@@ -38,58 +39,103 @@ function scaleAnimation () {
         }
     }
 
+    function firstanimation () {
+        icon_one = anime.timeline({
+            easing: 'easeInOutSine',
+        })
+        icon_one.add({
+            targets: '#satelite path',
+            strokeDashoffset: [anime.setDashoffset, 0],
+            endDelay: 500
+        })
+        .add({
+            targets: '#satelite_wing',
+            rotate: {
+                value: '0.5turn',
+                duration: 800,
+                easing: 'easeInOutSine'
+            },
+            // zIndex: {
+            //     value: [1, 5],
+            //     round: true
+            // },
+        })
+        .add({
+            targets: '#satelite_signal path',
+            opacity: [ 0, 1 ],
+            translateY: [ -20, 0 ],
+            translateX: [ -20, 0 ],
+            delay: anime.stagger(500)
+        })
+    }
 
+    function secondanimation () {
+        icon_two = anime.timeline({
+            easing: 'easeInOutSine',
+        })
+        icon_two.add({
+            targets: '#dandelion path',
+            strokeDashoffset: [anime.setDashoffset, 0],
+        })
+        .add({
+            targets: '.petal',
+            translateX: 60,
+            translateY: -60,
+            rotate: '15deg',
+            delay: anime.stagger(500)
+        })
+    }
     // var scaletimeline = anime.timeline({
     //     loop: true,
     // });
-    // anime({
+    // scaletimeline.
+    // add({
     //     targets: '.income-group .satellite',
     //     scale: [
     //         { value: 1, duration: 500 },
-    //         { value: 1.2, duration: 1800 },
+    //         { value: 1.2, duration: 800, endDelay: 4300 },
     //         { value: 1, duration: 500 }
     //     ]
-    // });
-    // anime({
+    // })
+    // .add({
     //     targets: '.income-group .dandelion',
     //     scale: [
-    //         { value: 1, duration: 500, delay: 2800 },
-    //         { value: 1.2, duration: 1800 },
+    //         { value: 1, duration: 500 },
+    //         { value: 1.2, duration: 800, endDelay: 4300 },
     //         { value: 1, duration: 500 }
     //     ]
-    // });
-    // anime({
+    // })
+    // .add({
     //     targets: '.income-group .rocket',
     //     scale: [
-    //         { value: 1, duration: 500,  delay: 5100},
-    //         { value: 1.2, duration: 1800 },
+    //         { value: 1, duration: 500 },
+    //         { value: 1.2, duration: 800, endDelay: 4300 },
     //         { value: 1, duration: 500 }
     //     ]
     // });
+
     // anime.remove('.income-group .income-group__item .satellite');
-    anime({ // 每個動畫放大、縮小
-        // targets: '#satelite path',
-        targets: '.income-group .income-group__item',
-        scale: [
-            { value: 1, duration: 500 },
-            { value: 1.2, duration: 800, endDelay: 4300 },
-            { value: 1, duration: 500 }
-        ],
-        strokeDashoffset: [anime.setDashoffset, 0],
-        duration: 8000,
-        loop: true,
-        delay: function (el, i, l) {
-            return i * 5000
-        }
-    });
-    // 衛星
-    anime({
-        targets: '#satelite path',
-        strokeDashoffset: [anime.setDashoffset, 0],
-        duration: 8000,
-        loop: true
-    })
+    // let scaltimeline = anime.timeline({
+    //     loop: true
+    // });
+    // scaltimeline.add({ // 每個動畫放大、縮小
+    //     targets: '.income-group .income-group__item',
+    //     scale: [
+    //         { value: 1, duration: 500 },
+    //         { value: 1.2, duration: 500, endDelay: 4300 },
+    //         { value: 1, duration: 500 }
+    //     ],
+    //     duration: 8000,
+    //     delay: function (el, i, l) {
+    //         console.log(el, i, l);
+    //         return i * 5000
+    //     }
+    // })
+    firstanimation();
+    secondanimation();
 }
+
+incomeAnimation();
 
 // var el = document.querySelector(".show");
 // let allsq = 70;
@@ -100,6 +146,4 @@ function scaleAnimation () {
 //     el.prepend(square);
 //     animeActive('square', '.show__item');
 // }
-
-animeActive('numRandom', '#demo-input');
-scaleAnimation();
+// animeActive('numRandom', '#demo-input');
